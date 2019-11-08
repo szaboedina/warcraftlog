7ae7d0c9f7d976d09f510f027689da2aace3f154
Edina Szabó <szabo.edina.1229@gmail.com>
2019-11-08 10:39:49 +0100

    Add project files.

---
+++ Jatekos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace warcraftlogs
+{
+    class Jatekos
+    {
+        int parse;
+        string nev;
+        int amount;
+        int iLvl;
+        int iLvlSzazalek;
+        double activeSzazalek;
+        double DPS;
+
+        public Jatekos(int parse, string nev, int amount, int iLvl, int iLvlSzazalek, double activeSzazalek, double dPS)
+        {
+            this.parse = parse;
+            this.nev = nev;
+            this.amount = amount;
+            this.iLvl = iLvl;
+            this.iLvlSzazalek = iLvlSzazalek;
+            this.activeSzazalek = activeSzazalek;
+            this.DPS = dPS;
+        }
+
+       /* public override string ToString()
+        {
+            return base.ToString();
+        }*/
+    }
+}
